e187de4596d6825607eeb0199a73c50dbfdd1313
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-10 00:04:54 +0000

    Add HTTP basic authentication to restrict visit /admin/products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV["ADMIN_AUTH_USERNAME"], password: ENV["ADMIN_AUTH_PSWD"]
 
   def index
     @products = Product.order(id: :desc).all
