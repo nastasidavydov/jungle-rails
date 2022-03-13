18d4d3bf4751a5851afea208960283260519b8b0
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-13 20:02:29 +0000

    Define finished, upcoming, active methods to check the status of sale and conditionally render it in sale partial

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,14 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    ends_on > Date.current
+  end
+
+  def active?
+    !finished? && !upcoming?
+  end
 end
