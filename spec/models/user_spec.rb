563a236d6e136724b7945df7399dc60dcdd8f795
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-15 01:37:56 +0000

    Add validation for email presence and uniqueness and test cases for it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   before(:each) do
-    @user = User.create!(first_name: "first name", last_name: "last_name", email: "example@mail.com", password: "password", password_confirmation: "password")
+    @user = User.create(first_name: "first name", last_name: "last_name", email: "example@mail.com", password: "password", password_confirmation: "password")
     
   end
   
@@ -23,6 +23,22 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
+
+    it "should fail without email" do
+      @user.email = nil
+
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "should fail if email is not unique" do
+      @user2 = User.create(first_name: "first name", last_name: "last_name", email: "EXAMPlE@mail.com", password: "password", password_confirmation: "password")
+
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+
   end
 
   describe '.authenticate_with_credentials' do
