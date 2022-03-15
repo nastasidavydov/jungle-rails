0a1610f666dadb06ec2afb9c7535fa4162ea4de2
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-15 01:25:37 +0000

    Add validation for first and last name to user module and test cases to check it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # gives User model authentication methods via bcrypt
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:each) do
+    @user = User.create!(first_name: "first name", last_name: "last_name", email: "example@mail.com", password: "password", password_confirmation: "password")
+    
+  end
+  
+
+  describe 'Validations' do
+    
+    it "should fail without first name" do
+      @user.first_name = nil
+      
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it "should fail without last name" do
+      @user.last_name = nil
+
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+end
