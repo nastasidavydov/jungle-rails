c27858c3a27a24d501f996ea7ce601a0fb617d78
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-15 02:35:49 +0000

    Add to find a user by email to be case insensitive and trimmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, pswd)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(pswd)
       user
     else
